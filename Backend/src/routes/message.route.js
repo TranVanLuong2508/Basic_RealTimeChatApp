bbd1be6c426269ac001ae2540532ee41029f8723
Tran Van Luong <tranvanluong032020@gmail.com>
2025-09-10 00:06:43 +0700

    Fix express import typo

--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -1,12 +1,11 @@
-import expess from "express"
+import express from "express"
 import { protectRoute } from "../middleware/auth.middleware.js"
 import { getUsersForSidebar, getMessage, sendMessage } from "../controllers/message.controller.js"
 
-const router = expess.Router()
+const router = express.Router()
 
 router.get("/users", protectRoute, getUsersForSidebar)
 router.get("/:id", protectRoute, getMessage)
-
 router.post("/send/:id", protectRoute, sendMessage)
 
 export default router
